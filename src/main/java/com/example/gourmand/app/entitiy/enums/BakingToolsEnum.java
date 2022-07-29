04287b3a8973d2c031835ce832d1fb85ed70c171
Mohsen <mohsen.s2012@yahoo.com>
2022-07-29 19:38:18 +0430

    add entities and models

---
+++ src/main/java/com/example/gourmand/app/entitiy/enums/BakingToolsEnum.java
@@ -0,0 +1,5 @@
+package com.example.gourmand.app.entitiy.enums;
+
+public enum BakingToolsEnum {
+    OVEN, GAS_STOVE, GRILL, BBQ, FRIER, OTHER
+}
---
+++ src/main/java/com/example/gourmand/app/entitiy/enums/RecipeCategoryEnum.java
@@ -0,0 +1,5 @@
+package com.example.gourmand.app.entitiy.enums;
+
+public enum RecipeCategoryEnum {
+    NONE, VEGETARIAN, HEALTHY, DIETARY, SEAFOOD, VEGAN
+}
