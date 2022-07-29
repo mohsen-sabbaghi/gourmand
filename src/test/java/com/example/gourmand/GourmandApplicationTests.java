4705809ce0bdedfd45cbc47c7fd0119736d1fc1d
Mohsen <mohsen.s2012@yahoo.com>
2022-07-29 19:25:30 +0430

    Initial commit

---
+++ src/test/java/com/example/gourmand/GourmandApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.gourmand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GourmandApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
